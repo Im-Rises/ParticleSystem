868f9379d824d758163eb669bec0a8c3a44892c4
Im-Rises <quentin-morel88@hotmail.com>
2022-12-05 14:18:21 -0500

    wip

--- ParticleEmission/ParticleEmissionLauncher.cpp
+++ ParticleEmission/ParticleEmissionLauncher.cpp
@@ -39,7 +39,7 @@ ParticleEmissionLauncher::ParticleEmissionLauncher() {
     // GL 3.2 + GLSL 150
     const char* glsl_version = "#version 150";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
 #else
