e46c2cadf3626c551004caf5bf2459de8a9ff5e0
Im-Rises <quentin-morel88@hotmail.com>
2022-12-05 14:04:02 -0500

    wip

--- ParticleEmission/Scene/Entity/ParticleGenerator/ParticleGenerator.cpp
+++ ParticleEmission/Scene/Entity/ParticleGenerator/ParticleGenerator.cpp
@@ -1,12 +1,46 @@
 #include "ParticleGenerator.h"
 
+#include <iostream>
+
 ParticleGenerator::ParticleGenerator() : Entity(
-        "ParticleGenerator.vert",
-        "ParticleGenerator.frag") {
+        "shaders/ParticleGenerator.vert",
+        "shaders/ParticleGenerator.frag") {
     init();
 }
 
 void ParticleGenerator::init() {
+    int index = 0;
+    float offset = 0.1f;
+    for (int y = -10; y < 10; y += 2) {
+        for (int x = -10; x < 10; x += 2) {
+            glm::vec2 translation;
+            translation.x = (float) x / 10.0f + offset;
+            translation.y = (float) y / 10.0f + offset;
+            translations[index++] = translation;
+        }
+    }
+
+    glGenBuffers(1, &instanceVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) (2 * sizeof(float)));
+    // also set instance data
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO); // this attribute comes from a different vertex buffer
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *) 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glVertexAttribDivisor(2, 1); // tell OpenGL this is an instanced vertex attribute.
 
 }
 
@@ -15,7 +49,8 @@ ParticleGenerator::~ParticleGenerator() {
 }
 
 void ParticleGenerator::destroy() {
-
+    glDeleteVertexArrays(1, &quadVAO);
+    glDeleteBuffers(1, &quadVBO);
 }
 
 void ParticleGenerator::update(float deltaTime) {
@@ -23,5 +58,8 @@ void ParticleGenerator::update(float deltaTime) {
 }
 
 void ParticleGenerator::render(glm::mat4 cameraViewMatrix, glm::mat4 cameraProjectionMatrix) {
-
+    shader.use();
+    glBindVertexArray(quadVAO);
+    glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100); // 100 triangles of 6 vertices each
+    glBindVertexArray(0);
 }
