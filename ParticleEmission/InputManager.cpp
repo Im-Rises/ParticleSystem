d3b171ef96930ea19bd474858872fc5549e4f661
Im-Rises <quentin-morel88@hotmail.com>
2022-12-15 23:41:09 -0500

    wip

--- ParticleEmission/InputManager.cpp
+++ ParticleEmission/InputManager.cpp
@@ -1,10 +1,28 @@
-//
-// Created by Quentin on 15/12/2022.
-//
-
 #include "InputManager.h"
 
 void InputManager::key_callback(GLFWwindow *window, int key, int scancode, int action, int mods) {
+//    auto* engine = (ParticleEngineLauncher*)glfwGetWindowUserPointer(window);
+
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
+
+//    switch (action)
+//    {
+//        case GLFW_PRESS: {
+//            keyPressed(window, key, engine);
+//            break;
+//        }
+//        case GLFW_RELEASE: {
+//            keyReleased(window, key, engine);
+//            break;
+//        }
+//        case GLFW_REPEAT: {
+//            keyRepeated(window, key, engine);
+//            break;
+//        }
+//        default: {
+//            std::cout << "Unknown keyboard action" << std::endl;
+//            break;
+//        }
+//    }
 }
