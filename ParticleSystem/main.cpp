f00b36e6d47acdaafd1a073acb6ee59450ac4e95
Im-Rises <quentin-morel88@hotmail.com>
2023-02-04 12:33:02 -0500

    wip

--- ParticleSystem/main.cpp
+++ ParticleSystem/main.cpp
@@ -1,6 +1,10 @@
 ﻿#include "ParticleSystemLauncher.h"
+#include <iostream>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
+    std::cout << PROJECT_NAME << " " << PROJECT_VERSION << std::endl;
+    std::cout << PROJECT_GITHUB << std::endl;
+    std::cout << PROJECT_AUTHOR << std::endl;
     ParticleSystemLauncher app;
     app.start();
     return 0;
