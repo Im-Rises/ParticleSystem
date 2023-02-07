dfacb8d551120077c9dd7a80ba1f70ced0c1d116
Im-Rises <quentin-morel88@hotmail.com>
2023-02-07 13:43:59 -0500

    Updated GL draw method

--- ParticleSystem/Scene/Entity/ParticleGenerator/ParticleGeneratorBillboard.cpp
+++ ParticleSystem/Scene/Entity/ParticleGenerator/ParticleGeneratorBillboard.cpp
@@ -19,7 +19,7 @@ ParticleGeneratorBillboard::ParticleGeneratorBillboard(int maxParticles) : Entit
 void ParticleGeneratorBillboard::create() {
     glGenBuffers(1, &instanceVBO);
     glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Particle) * particles.size(), particles.data(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Particle) * particles.size(), particles.data(), GL_DYNAMIC_DRAW);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     glGenVertexArrays(1, &quadVAO);
@@ -32,24 +32,7 @@ void ParticleGeneratorBillboard::create() {
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadEBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
-
-    //    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    //    glEnableVertexAttribArray(0);
-    //
-    //    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO); // this attribute comes from a different vertex buffer
-    //    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    //    glEnableVertexAttribArray(1);
-    //
-    //    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    //    glEnableVertexAttribArray(2);
-    //
-    //    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float)));
-    //    glEnableVertexAttribArray(3);
-    //
-    //    glVertexAttribDivisor(1, 1); // tell OpenGL this is an instanced vertex attribute.
-    //    glVertexAttribDivisor(2, 1);
-    //    glVertexAttribDivisor(3, 1);
-
+    
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
